bb75a9d40d51ff3956d84f88bb65920ded5912fa
nidaloveuse <6902@holbertonstudents.com>
2023-06-18 06:45:08 -0700

    fix task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - main fonction
-*return: 0 if ok else other
+*return: O if ok else other
 */
 int main (void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return (o);
+  return (0);
 }
