232968ae00e88bcec764d7e751b5bb612596b0b2
nidaloveuse <6902@holbertonstudents.com>
2023-06-21 04:59:33 -0700

    Adding 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**                                                                                                                                                                                                                
+* main - Prints the size of various types based on                                                                                                                                                                 
+*        the computer it is compiled and run on..                                                                                                                                                                  
+*                                                                                                                                                                                                                  
+* Return: Always 0.                                                                                                                                                                                                
+*/
+
+int main() {
+  char number;
+
+  /* Imprime les nombres dans l'ordre */
+  for (number = '0'; number <= '9'; number++) {
+    putchar (number);
+  }
+
+  /* Imprime une nouvelle ligne */
+  putchar ('\n');
+
+  return (0);
+}
