c325eeded6e143f942e1990a7a51258729f52c70
nidaloveuse <6902@holbertonstudents.com>
2023-06-21 07:11:11 -0700

    Adding 9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Prints the size of various types based on
+*        the computer it is compiled and run on..
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+  int number;
+
+  /* imprime les chiffres dans l'ordres numérique */
+  for (number = 48 ; number < 58 ; number++)
+  {
+    putchar(number);
+    if (number != 57)
+    {
+      putchar(',');
+      putchar(' ');
+    }
+  }  
+  putchar('\n');
+
+  return (0);
+  
+}
