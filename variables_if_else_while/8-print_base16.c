8308577c7e3b83500a39660356197954a945975d
nidaloveuse <6902@holbertonstudents.com>
2023-06-21 06:27:01 -0700

    Adding 8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**                                                                                                                                                                                                                
+* main - Prints the size of various types based on                                                                                                                                                                 
+*        the computer it is compiled and run on..                                                                                                                                                                  
+*                                                                                                                                                                                                                  
+* Return: Always 0.                                                                                                                                                                                                
+*/
+
+int main (void)
+{
+  char letter;
+  int number;
+
+  /* imprimer l'alphabet en minuscule */
+  for(letter = 'a'; letter <= 'f'; letter++) {
+    putchar(letter);
+  }
+
+  /* imprime les chiffres dans l'ordres numérique */
+  for (number = '0'; number <= '9'; number++) {
+    putchar(number);
+  }
+
+  /* imprime une nouvelle ligne */
+  putchar('\n');
+
+  return (0);
+
+}
