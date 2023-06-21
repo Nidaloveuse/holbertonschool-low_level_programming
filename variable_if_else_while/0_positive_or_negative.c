7a3ad155d884d3107144bba2c848a623da096a85
nidaloveuse <6902@holbertonstudents.com>
2023-06-21 00:22:25 -0700

    fix task

--- variable_if_else_while/0_positive_or_negative.c
+++ variable_if_else_while/0_positive_or_negative.c
@@ -2,7 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
+/**
+* main - Prints the size of various types based on
+*        the computer it is compiled and run on..
+*
+* Return: Always 0.
+*/
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -11,17 +16,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	  {
-	    printf("%d is positive\n", n);
-	  }
-	else if(n==0)
-	  {
-	    printf("%d is zero\n", n);
-	  }
+	if (n > 0)
+	{
+	  printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	  printf("0 is zero\n");
+	}
 	else
-	  {
-	    printf("%d is negative\n", n);
-	  }
+	{
+	  printf("%d is negative\n", n);
+	}
 	return (0);
 }
