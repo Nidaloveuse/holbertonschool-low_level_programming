970330b4163e21e88c01f00b61e28a9dee6ed89d
nidaloveuse <6902@holbertonstudents.com>
2023-06-21 01:12:39 -0700

    Adding 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,14 @@
 int main() {
     char letter;
 
-    // Imprimer l'alphabet en minuscules
     for (letter = 'a'; letter <= 'z'; letter++)
       {
         putchar(letter);
       }
-
-    // Imprimer l'alphabet en majuscules
     for (letter = 'A'; letter <= 'Z'; letter++)
       {
         putchar(letter);
       }
-
-    // Imprimer une nouvelle ligne
     putchar('\n');
 
     return 0;
