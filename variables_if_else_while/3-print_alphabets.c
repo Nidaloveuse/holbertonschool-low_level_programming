3f4f9077f3564dd727b159f57e01f02e699572f1
nidaloveuse <6902@holbertonstudents.com>
2023-06-21 01:15:05 -0700

    Adding 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+* main - Prints the size of various types based on
+*        the computer it is compiled and run on..
+*
+* Return: Always 0.
+*/
 int main() {
     char letter;
 
