cb804b643e219752f08e62c522fd5f337bcaecc7
nidaloveuse <6902@holbertonstudents.com>
2023-06-21 04:38:23 -0700

    fix task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Prints the size of various types based on
+*        the computer it is compiled and run on..
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+  char letter;
+
+  /* imprime l'alphabet en lettre minuscule */
+      for (letter = 'a'; letter <= 'z'; letter++)
+    {
+      if (letter != 'e' && letter != 'q')
+	putchar (letter);
+    }
+  /* imprime une nouvelle ligne */
+      putchar ('\n');
+      return (0);
+}
---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Prints the size of various types based on
+*        the computer it is compiled and run on..
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+  char number;
+
+  /* Imprime moi les chiffres dans l'ordre numérique */
+  for (number = '0'; number <= '9'; number++)
+  {
+    putchar(number);
+  }
+
+  /* Imprime moi une nouvelle ligne */
+  putchar('\n');
+
+  return (0);
+}
